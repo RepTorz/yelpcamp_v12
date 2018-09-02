1a4516964526d74c74e3d52a2b5942fcf08e7656
Tony Wong <twwon17@gmail.com>
2018-09-02 13:12:57 +0000

    update dburl

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ var commentRoutes       = require("./routes/comments"),
     campgroundRoutes    = require("./routes/campgrounds"),
     indexRoutes         = require("./routes/index")
 
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://tony:pokeds19@ds263460.mlab.com:63460/yelpcamp");
 
-mongoose.connect("mongodb://tony:pokeds19@ds263460.mlab.com:63460/yelpcamp");
-// mongodb://tony:pokeds19@ds263460.mlab.com:63460/yelpcamp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
